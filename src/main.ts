f13068df669119140e098b1f150eedc6397130ac
Momna Javed <maimuna.javed@gigalabs.co>
2025-02-14 17:05:43 +0500

    HTTP-Exception

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,9 @@ import * as dotenv from 'dotenv';
 dotenv.config();
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
   app.enableCors();
   await app.listen(3000);
 }
